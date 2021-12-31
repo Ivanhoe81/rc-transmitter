e34576b688c7ee96c4bb1f41490410b2693b9b1d
Ivanhoe81 <42270386+Ivanhoe81@users.noreply.github.com>
2021-12-31 13:10:05 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+    radio.sendValue("duda", ki)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
+    radio.sendValue("duda", be)
+})
 let y = 0
 let x = 0
+let be = 0
+let ki = 0
 joystickbit.initJoystickBit()
 radio.setGroup(1)
 basic.showIcon(IconNames.SmallHeart)
+ki = 0
+be = 1
 basic.forever(function () {
-    x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 0, 1023, -100, 100)
-    y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 0, 1023, -100, 100)
+    x = Math.map(Math.map(0, 0, 1023, 100, -100), 0, 1023, 100, -100)
+    y = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.Y), 0, 1023, 100, -100)
     radio.sendValue("x", x)
     radio.sendValue("y", y)
 })
