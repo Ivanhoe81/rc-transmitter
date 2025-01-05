885865297980b0903bec3d467811586d787240ee
Ivanhoe81 <42270386+Ivanhoe81@users.noreply.github.com>
2025-01-05 10:56:26 +0100

    pxt.json, main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.up, function () {
+    radio.sendValue("bal", ki)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
+    radio.sendValue("le", be)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.up, function () {
+    radio.sendValue("fel", ki)
+})
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
     radio.sendValue("jobb", be)
 })
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.up, function () {
+    radio.sendValue("jobb", ki)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.up, function () {
+    radio.sendValue("le", ki)
+})
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
-    radio.sendValue("duda", be)
+    radio.sendValue("fel", be)
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
     radio.sendValue("bal", be)
@@ -10,10 +25,11 @@ joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType
 let y = 0
 let x = 0
 let be = 0
+let ki = 0
 joystickbit.initJoystickBit()
 radio.setGroup(1)
 basic.showIcon(IconNames.SmallHeart)
-let ki = 0
+ki = 0
 be = 1
 basic.forever(function () {
     x = Math.map(joystickbit.getRockerValue(joystickbit.rockerType.X), 0, 1023, 165, 245)
